7990748451e547ef9c8a34f090357d6ba5f91ef1
MalinaMan <MalinaMan>
2026-01-17 12:35:10 +0200

    process.env

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,8 @@
 import mysql from "mysql2/promise";
 
 export const db = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "eneba"
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
 });
